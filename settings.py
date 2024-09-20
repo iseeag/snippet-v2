ec674e94aa022de642e4069cd52b79a71c0283cd
Maximillion <iseeag@hotmail.com>
2024-09-20 15:56:25 +0800

    increase MAX_ROW_INFER to 2000

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    MAX_ROW_INFER: int = 500
+    MAX_ROW_INFER: int = 2000
     oai_model: str = 'gpt-4o'
     oai_key: str = 'sk-6jpejlScJXs1zpv6369841E107C54d999dEc953b55350343'
     oai_url: str = 'https://key.agthub.ai/v1'
